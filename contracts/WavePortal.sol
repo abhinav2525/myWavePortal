9398cc52cc1c9764a4dd2468082ed15dac4c6568
Abhinav Krishna <56315478+abhinav2525@users.noreply.github.com>
2022-07-16 23:28:37 +0530

    modified the code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,8 +6,9 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
 
-    event NewWave( address indexed from, uint256 timestamp, string message);
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {
         address waver;
@@ -17,37 +18,62 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() payable{
-        console.log("Yo yo, I am a contract and I am smart");
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+    constructor() payable {
+        console.log("We have been constructed!");
+        /*
+         * Set the initial seed
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
-        totalWaves += 1;
-        console.log("%s waved w/ message %s", msg.sender, _message);
-
-        waves.push(Wave(msg.sender,_message,block.timestamp));
-        
-        emit NewWave(msg.sender, block.timestamp, _message);
-
-        uint256 prizeAmount = 0.0001 ether;
-
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
         require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 10sec"
         );
 
-        (bool success, )= (msg.sender).call{value:prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
 
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than they contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
-    function getAllWaves() public view returns (Wave[] memory){
+    function getAllWaves() public view returns (Wave[] memory) {
         return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -2,20 +2,26 @@ const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy({
         value: hre.ethers.utils.parseEther("0.1"),
-    }
-
-    );
+    });
+    await waveContract.deployed();
+    console.log("Contract addy:", waveContract.address);
 
     let contractBalance = await hre.ethers.provider.getBalance(
         waveContract.address
     );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
-
-    /**
-     * Let's send a few waves!
+    /*
+     * Let's try two waves now
      */
-    let waveTxn = await waveContract.wave("A message!");
-    await waveTxn.wait(); // Wait for the transaction to be mined
+    const waveTxn = await waveContract.wave("This is wave #1");
+    await waveTxn.wait();
+
+    const waveTxn2 = await waveContract.wave("This is wave #2");
+    await waveTxn2.wait();
 
     contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
     console.log(
